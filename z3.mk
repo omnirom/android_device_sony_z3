5e3b0dab2785c20f4bc96d59706fee39901e6843
nailyk-fr <nailyk_git@nailyk.fr>
2017-10-28 16:33:41 +0200

    z3: build: Rename device [2/2]
     * Rename the vendor repo and defconfig
    
    Change-Id: I96618de148d863b106dcb9fbeeee1b651abe4c26

--- z3.mk
+++ z3.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/sony/shinano-common/shinano_omni.mk)
 
 # Include non-opensource parts
-$(call inherit-product, vendor/sony/shinano-leo-caf/leo-vendor.mk)
+$(call inherit-product, vendor/sony/shinano-z3-caf/z3-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/sony/z3/overlay
