06299f8603fcecb3602a70ef9f0acbf2c59e9606
mcgi5sr2 <mcgi5sr2@gmail.com>
2018-01-01 15:55:57 +0000

    Commonise Audio to Shinano - 	move audio xmls to vendor
    Change-Id: I7f64b7af05276533828b64f1b93969b3a72dd695

---
+++ product/Audio.mk
@@ -0,0 +1,4 @@
+# Audio configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:vendor/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:vendor/etc/mixer_paths_auxpcm.xml
